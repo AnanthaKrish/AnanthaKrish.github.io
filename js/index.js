154fefc07703878a09b6b9be866e62c63c1f4d96
Ananth <agirijak@in.ibm.com>
2018-10-28 23:05:17 +0530

    New website

---
+++ js/index.js
@@ -0,0 +1,29 @@
+//  $(document).ready(function() {
+
+//         // Transition effect for navbar and back-to-top icon
+//         $(window).scroll(function() {
+//           // checks if window is scrolled more than 500px, adds/removes solid class
+//           if($(this).scrollTop() > 550) { 
+//               $('.navbar').addClass('solid');
+//               $('.back-to-top').addClass('visible'); 
+//           } else {
+//               $('.navbar').removeClass('solid');
+//               $('.back-to-top').removeClass('visible');  
+//           }
+
+// //         });
+          
+// $(function() {
+//     // Bind Click event to the drop down navigation button
+//     $(".nav-button").click(function() {
+//       /*  Toggle the CSS closed class which reduces the height of the UL thus 
+//           hiding all LI apart from the first */
+//       $(this).parent().parent().toggleClass("closed");
+//     });
+  
+//   });
+//       });
+
+
+
+  
